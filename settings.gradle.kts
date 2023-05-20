6a5e507b463d173060aa8ae16c673d4f95d3c2b5
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-05-20 20:24:28 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TMDA"
+include(":app")
+include(":movies")
+include(":tv")
+include(":authentication")
+include(":account")
+include(":core")
